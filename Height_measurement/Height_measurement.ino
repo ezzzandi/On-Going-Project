a8f09774a72ccab6ff7f2ae59be0cf0adfc3eeb0
T. Ezandi Trinanda <ezzzandi@gmail.com>
2024-02-26 14:30:44 +0700

    Update Height_measurement.ino
    Using NewPing library to make more simple and shorter code

--- Height_measurement/Height_measurement.ino
+++ Height_measurement/Height_measurement.ino
@@ -1,20 +1,17 @@
 #include <LiquidCrystal_I2C.h>
+#include <NewPing.h>
 //define I2C address 0x27, 2 line
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define TRIG 3
+#define ECHO 4
+#define MAX_DISTANCE 200
+
 long cm = 0;
 long tinggiBadan;
-long readUltrasonicDistance(int trig, int echo) {
-  pinMode(trig, OUTPUT);  // Clear the trigger
-  digitalWrite(trig, LOW);
-  delayMicroseconds(5);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  pinMode(echo, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echo, HIGH);
-}
+
+NewPing sonar(TRIG, ECHO, MAX_DISTANCE);
+
 void setup()
 {
   Serial.begin(9600);
@@ -24,17 +21,13 @@ void setup()
 }
 void loop()
 {
-  // measure the ping time in cm
-  cm = (readUltrasonicDistance(3, 4) / 2) / 29.1;
-  Serial.print(cm);
-  Serial.println("cm");
+  cm = sonar.ping_cm();
   tinggiBadan = 200 - cm;
   lcd.setCursor(0, 0);
   lcd.print("Tinggi badan:");
   lcd.setCursor(0, 1);
   lcd.print(tinggiBadan);
   lcd.setCursor(3, 1);
-  lcd.print("tinggiBadan");
   delay(500); // Wait for 500 millisecond(s)
   lcd.clear();
 }
